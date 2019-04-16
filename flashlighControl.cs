dfbe97ada23d3bd05df88f16e86da623222433e9
Nicolas2323 <42986955+Nicolas2323@users.noreply.github.com>
2019-04-16 00:20:38 -0400

    week 4

---
+++ flashlighControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlighControl : MonoBehaviour
+{
+    private Light myLight;
+
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
+    }
+}
