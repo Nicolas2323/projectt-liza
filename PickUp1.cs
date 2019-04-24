8a64007cc3ce2cf49ec507de473c51077ce9d42c
Nicolas2323 <42986955+Nicolas2323@users.noreply.github.com>
2019-04-24 00:57:14 -0400

    week 7

---
+++ PickUp1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp1 : MonoBehaviour
+{
+    public Transform theDest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetComponent<Rigidbody>().transform.localScale = Vector3(1.0f, 1.0f, 1.0f);
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+            this.transform.position = theDest.position;
+            this.transform.parent = GameObject.Find("destination").transform;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            //GetComponent<Rigidbody>().transform.localScale = Vector3(1.0f, 1.0f, 1.0f);
+            this.transform.parent = null;
+            GetComponent<Rigidbody>().useGravity = true;
+        }
+
+    }
+
+    //private Vector3 Vector3(float v1, float v2, float v3)
+    //{
+    //    throw new NotImplementedException();
+    //}
+}
